4f66b5dc5f6b1df52f72cec44a575f0e54c42bf9
Pyry <pyry.satama@aalto.fi>
2023-08-02 13:36:57 +0300

    started project

---
+++ songgen.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import string
+import re
+
+# hyperparameters
+n_features = 18
+n_heads = 6
+n_layers = 2
+dropout=0.2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+block_size = 8
+batch_size = 4
+max_new_tokens = 2
+
+with open('eminem.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+punctuation = string.punctuation.replace("'", "")
+translator = str.maketrans('', '', punctuation)
+text = re.sub(r'\(.*?\)', '', text)
+text = text.translate(translator)
+words = list(set(text.split()))
+vocab_size = len(words)
+
+stoi = {w:i for i,w in enumerate(words)} # mapping from words to integers
+itos = {i:w for i,w in enumerate(words)} # mapping from integers to words
+encode = lambda s: [stoi[c] for c in s.split()] # take a string, output a list of integers
+decode = lambda l: ' '.join(itos[i] for i in l) # take a list of integers, output a string
+
+data = torch.tensor(encode(text), dtype=torch.long)
+
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split=='train' else val_data
+    ix = torch.randint(len(data)-block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+
+class Block(nn.Module):
+    
+    def __init__(self, n_features, n_heads):
+        super().__init__()
+        self.mha = nn.MultiheadAttention(n_features, n_heads)
+        self.ln1 = nn.LayerNorm(n_features)
+        
+        self.ff = nn.Sequential(
+            nn.Linear(n_features, 4 * n_features),
+            nn.ReLU(),
+            nn.Linear(4 * n_features, n_features),
+        )
+        self.ln2 = nn.LayerNorm(n_features)
+        
+    def forward(self, x):
+        x = x.permute(1, 0, 2)  # permute to match (seq_len, batch_size, embedding_dim)
+        attn_output, _ = self.mha(self.ln1(x), self.ln1(x), self.ln1(x))  # attn_output has the same shape as x
+        x = x + attn_output
+        x = x.permute(1, 0, 2)  # permute back to (batch_size, seq_len, embedding_dim)
+        x = x + self.ff(self.ln2(x))
+        return x  
+
+class SongGenerator(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.embedding = nn.Embedding(vocab_size, n_features)
+        
+        self.pos_encoding = nn.Embedding(block_size, n_features)
+        self.blocks = nn.Sequential(*[Block(n_features, n_heads) for _ in range(n_layers)])
+        self.ln_f = nn.LayerNorm(n_features) # final layer norm
+        self.lm_head = nn.Linear(n_features, vocab_size)
+
+        
+    def forward(self, idx, targets=None):
+        
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.embedding(idx) # (B,T,C)
+        pos_emb = self.pos_encoding(torch.arange(T, device=device)) # (T, C)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            #append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
